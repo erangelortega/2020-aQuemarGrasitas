b4271f55c47e64531a638f765eb0dbc794d40b5f
erangelortega <elianarangelortega@gmail.com>
2020-06-06 18:28:45 -0300

    PUNTO 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,11 @@ laVerdad = True
 {-Modelado del gimnasta
 De cada gimnasta nos interesa saber su edad, su peso y su coeficiente de tonificación.-}
 
+type Nombre = String
+type Edad = Float
+type Peso = Float
+type CoeficienteDeTonificacion = Float
+
 data Gimnasta = UnGimnasta {
     nombre :: String,
     edad :: Float,
@@ -25,11 +30,33 @@ relax minutos gimnasta = gimnasta
 -- PUNTO 1 ---
 {-Saber si alguien está saludable, lo cual se cumple si no está obeso y tiene una tonificación mayor a 5. Alguien es obeso si pesa más de 100 kilos. -}
 
-Saludable :: Gimnasta -> Bool
-estaSaludable gimnasta = (not . obeso) gimnasta && tonificado gimnasta
+saludable :: Gimnasta -> Bool
+saludable gimnasta = (not . obeso) gimnasta && tonificado gimnasta
 
 obeso :: Gimnasta -> Bool
 obeso gimnasta = peso gimnasta > 100
 
 tonificado :: Gimnasta -> Bool
 tonificado gimanasta = coeficienteDeTonificacion gimanasta > 5
+
+
+-- PUNTO 2 --
+{-Hacer que el gimnasta queme una cantidad de calorías, lo que produce que baje de peso.-}
+type Calorias = Float
+
+quemarCalorias :: Gimnasta -> Calorias -> Gimnasta
+quemarCalorias gimnasta calorias 
+    | obeso gimnasta = perderPeso (calorias / 150) gimnasta
+    | (not . obeso) gimnasta && edadMayorA 30 gimnasta && (calorias > 200 )= perderPeso 1 gimnasta
+    | otherwise = perderPeso (calorias /(peso gimnasta * edad gimnasta)) gimnasta
+
+
+perderPeso :: Peso -> Gimnasta -> Gimnasta
+perderPeso pesoAperder gimnasta = gimnasta {peso = peso gimnasta - pesoAperder}
+
+edadMayorA :: Float -> Gimnasta -> Bool
+edadMayorA num gimnasta = edad gimnasta > num
+
+-- PUNTO 3 -- 
+{-La cinta quema calorías en función de la velocidad promedio alcanzada durante el ejercicio, quemando 1 caloría por la velocidad promedio por minuto.
+La caminata es un ejercicio en cinta con velocidad constante de 5 km/h. -}
